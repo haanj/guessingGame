86f4f18da461779cd8ff95efceeadeca4a111ac8
haanj <joshua.haan@ymail.com>
2015-10-31 15:02:37 -0700

    add basic input answer correction

--- game.js
+++ game.js
@@ -2,16 +2,58 @@
 var correctCount = 0
 
 //questionArray stores pairs of questions and answers. Any number of Q/A pairs can be added/removed from here without breaking the script. For best results, keep the answers lowercase
-var questionArray = [["Do I have a PS4?", "no"], ["Do I like to play disc golf?", "yes"], ["Am I good at disc golf?", "no"]]
+var questionArray = [
+  ["Do I have a PS4?", "no"],
+  ["Do I like to play disc golf?", "yes"],
+  ["Am I good at disc golf?", "no"],
+  ["Do I enjoy playing Eldritch Horror?", "yes"]
+  //Add more questions in ['question', 'yes/no'] format here
+]
+
+
+//answerErrorCorrect will standardize user's yes/no answer
+function answerErrorCorrect(ans) {
+  var ansL = ans.toLowerCase(); //lowercases answer
+
+  if (
+      ansL === "yes"  ||
+      ansL === "y"    ||
+      ansL === "affirmative"  ||
+      ansL === "ys"   ||
+      ansL === "si"   ||
+      ansL === "oui"  ||
+      ansL === "ouais" ||
+      ansL === "yes!"
+      ) {
+    return "yes"; //returns yes if user types in "yes, with some variations allowed"
+
+  } else if (
+      ansL === "no"  ||
+      ansL === "n"    ||
+      ansL === "negative"  ||
+      ansL === "non"  ||
+      ansL === "ouais" ||
+      ansL === "no!"
+      ) {
+    return "no"; //returns "no" if user types "no", with some variations allowed"
+  } else {
+    alert("I don't understand your answer and will have to count it wrong.")
+    return "WRONG"
+  }
+}
+
 
 var userName = prompt("Hello, what is your name?", "Type your name here");
 
 alert("Hello " + userName + ", are you ready to play a game? How well do you know me?");
 
 
+
+
 //This for loop runs through the questionArray, checking user's input against the answer
 for(i=0; i<questionArray.length; i++) {
   var answer = prompt(questionArray[i][0], "yes/no");
+  answer = answerErrorCorrect(answer);
   if (answer === questionArray[i][1]){
     alert("Correct!");
     correctCount++;
