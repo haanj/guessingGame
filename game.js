d165814ab86b8416701bc206a8aeeaf316e8913b
haanj <joshua.haan@ymail.com>
2015-11-01 09:58:44 -0800

    add functionGame function

--- game.js
+++ game.js
@@ -1,91 +1,50 @@
+var userName;
 var randomNumber = Math.floor(Math.random() * 10) + 1;
-var correctCount = 0
+var correctCount = 0;
 var questionArray = [
   ["Do I live in Seattle?", "no", "I live out in Fall City."],
   ["Do I like to ski?", "yes", "I do like to ski. I hope the season's better this year."],
   ["Am I good at disc golf?", "no", "I like to play, but I'm absolutely terrible at it."],
   ["Do I enjoy playing Eldritch Horror?", "yes", "I do enjoy Eldritch Horror; it's probably one of my favorites."]
-  //Add more questions here, if desired.
-]
+];
 
-
-//This for loop runs through the questionArray, checking user's input against the answer
-/*
-  for(i=0; i<questionArray.length; i++) {
-  var answer = prompt(questionArray[i][0], "yes/no");
-  answer = answerErrorCorrect(answer); //runs answer through basic error correction function
-  if (answer === questionArray[i][1]){
-    alert("Correct! " + questionArray[i][2]);
-    correctCount++;
+//prompts with question, checks user's input against answer, responds accordingly
+function functionGame(question, answer, response) {
+  var userAnswer = answerErrorCorrect(prompt(question, "yes/no"));
+  if (userAnswer === answer){
+    alert("Correct! " + response);
+    correctCount++
   } else {
-    alert("I'm sorry " + userName + ", that's incorrect. " + questionArray[i][2] + ".");
+    alert("I'm sorry " + userName + ", that's incorrect. " + response + ".");
   }
 }
 
-*/
-
-
-
-
-
-function functionGame(question, answer, response) {
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //answerErrorCorrect will standardize user's yes/no answer
 function answerErrorCorrect(ans) {
-  var ansL = ans.toLowerCase(); //lowercases answer
+  var ansL = ans.toLowerCase();
 
-  if (ansL === "yes" || ansL === "y") {
-    return "yes"; //returns yes if user types in "yes, with some variations allowed"
-  } else if (ansL === "no" || ansL === "n") {
-    return "no"; //returns "no" if user types "no", with some variations allowed"
-  } else {
+  if (ansL === "yes" || ansL === "y")
+    {return "yes";}
+  else if (ansL === "no" || ansL === "n")
+    {return "no";}
+  else {
     alert("I don't understand your answer and will have to count it wrong.")
     return "WRONG"
   }
 }
 
 
-var userName = prompt("Hello, what is your name?", "Type your name here");
-
-if (userName === "Rick" || userName === "rick") {
-  alert("Alright! Let's get schwifty!")
-} else {
+userName = prompt("Hello, what is your name?", "Type your name here");
 alert("Hello " + userName + ", are you ready to play a game? How well do you know me?");
-}
 
 
-//This for loop runs through the questionArray, checking user's input against the answer
 for(i=0; i<questionArray.length; i++) {
   functionGame(questionArray[i][0], questionArray[i][1], questionArray[i][2])
-  }
 }
 
-//After the questions have all been asked, the user is alerted the number o correct answers that they got
 
 alert("Okay, " + userName + ", it looks like you've gotten " + correctCount + " answer(s) right out of " + questionArray.length + ".");
 
-//Personalized response based off number of correct answers
 
 if (correctCount === 0) {
   alert("I can't believe you didn't get a single question right! I'm disappointed in you.");
@@ -93,7 +52,10 @@ if (correctCount === 0) {
   alert("You got all the questions correct! Incredible!");
 }
 
+
+
 //bonus work.
+
 var numberGuess = parseInt(prompt("Bonus round: I'm thinking of a number between 1 and 10. Can you guess what it is?", "Enter your guess here. Numeric answers only, please."));
 
 while (numberGuess !== randomNumber) {
@@ -108,5 +70,6 @@ while (numberGuess !== randomNumber) {
   }
 }
 alert("Correct! Wow, " + userName + ", you're a great guesser!");
+
 
 
