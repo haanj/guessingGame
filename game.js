ca78809532a5e0e790699abad1253b30c49c39d7
haanj <joshua.haan@ymail.com>
2015-11-08 09:23:39 -0800

    added some basic DOM

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
+//document.getElementById(hidingSpace).style.color = "red";
+
+
 var userName;
-var userAnswer;
 var correctCount = 0;
 var questionArray = [
   ["Do I live in Seattle?", "no", "I live out in Fall City."],
   ["Do I like to ski?", "yes", "I do like to ski. I hope the season's better this year."],
   ["Do I enjoy playing Eldritch Horror?", "yes", "I do enjoy Eldritch Horror; it's probably one of my favorites."],
-  ["", "no", "I am not Irish."]
+  ["Am I Irish?", "no", "I am not Irish."]
 ];
 var numberGuessArray = [
   ["I'm thinking of a number between 1 and 10. Can you guess what it is?", (Math.floor(Math.random() * 10) + 1)],
@@ -14,12 +16,15 @@ var numberGuessArray = [
 
 //prompts with question, checks user's input against answer, responds accordingly
 function functionGame(question, answer, response) {
-  userAnswer = answerErrorCorrect(prompt(question, "yes/no"));
+
+  var userAnswer = answerErrorCorrect(prompt(question, "yes/no"));
   if (userAnswer === answer){
-    alert("Correct! " + response);
+    document.getElementById("question").innerHTML = question;
+    document.getElementById("answer").innerHTML = ("Correct! " + response);
     correctCount++
   } else {
-    alert("I'm sorry " + userName + ", that's incorrect. " + response + ".");
+    document.getElementById("question").innerHTML = question;
+    document.getElementById("answer").innerHTML = ("I'm sorry " + userName + ", that's incorrect. " + response + ".");
   }
 }
 
@@ -58,12 +63,22 @@ function numberGuess(userNumber, ansNumber) {
       userNumber = parseInt(prompt("That's not even a number! Try again."));
     }
   }
-  alert("Correct! Wow, " + userName + ", you're a great guesser!");
+  document.getElementById("answer").innerHTML = ("Correct! Wow, " + userName + ", you're a great guesser!");
 }
 
+//gets user's name
+function getUserName() {
+  var name = prompt("Hello, what is your name?", "Type your name here");
+  alert("Hello " + name + ", are you ready to play a game? How well do you know me?");
+  return name;
+}
+
+
+
+
+
 //////SCRIPT START//////
-userName = prompt("Hello, what is your name?", "Type your name here");
-alert("Hello " + userName + ", are you ready to play a game? How well do you know me?");
+userName = getUserName();
 
 for(i=0; i<questionArray.length; i++) {
   functionGame(questionArray[i][0], questionArray[i][1], questionArray[i][2])
